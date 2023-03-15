3b9defe01631d95f92d7f852388a6dded0fec1d3
André Albuquerque <40603968+alcavalcanti@users.noreply.github.com>
2023-03-15 16:48:17 -0300

    Adding new project
    Adding new project for the 2nd clojure course module

---
+++ loja/src/loja/aula1.clj
@@ -0,0 +1,40 @@
+(ns loja.aula1)
+;["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"]
+;{"guilherme" 37, "paulo" 39}
+;'(1 2 3 4 5 6)
+;[[0 1]]
+; #{}
+
+;map
+;reduce
+;filter
+
+(map println ["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"])
+(println (first ["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"]))
+(println (rest ["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"]))
+(println (next ["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"]))
+(println (seq [1 2 3 4 5]))
+
+
+(defn meu-mapa [funcao sequencia]
+      (let [primeiro (first sequencia)]
+        (if primeiro
+          (do (funcao primeiro)
+              (meu-mapa funcao (rest sequencia))))))
+
+(println "MEU MAPA STOP FALSE")
+(meu-mapa println ["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"])
+(meu-mapa println ["daniela" false "carlos" "paulo" "lucia" "ana"])
+
+
+(defn meu-mapa [funcao sequencia]
+      (let [primeiro (first sequencia)]
+        (if (not (nil? primeiro))
+          (do (funcao primeiro)
+              (meu-mapa funcao (rest sequencia))))))
+
+(println "MEU MAPA STOP NIL")
+(meu-mapa println ["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"])
+(meu-mapa println ["daniela" false "carlos" "paulo" "lucia" "ana"])
+
+(meu-mapa println (range 100000))
