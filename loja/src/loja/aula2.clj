4c1822884619112f81baaf77e5ff5a2ac8b809c2
André Albuquerque <40603968+alcavalcanti@users.noreply.github.com>
2023-03-16 09:01:36 -0300

    Update aula1 and aula2
    Update aula1 and aula2 finishing the recent lessons of the course

--- loja/src/loja/aula2.clj
+++ loja/src/loja/aula2.clj
@@ -1,1 +1,35 @@
 (ns loja.aula2)
+
+;["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"]
+
+(defn conta
+      ([elementos] (conta 0 elementos))
+      ([total-ate-agora elementos]
+       ;(println total-ate-agora elementos)
+       (if (seq elementos)
+         (recur (inc total-ate-agora) (next elementos))
+         total-ate-agora)))
+
+(println (conta ["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"]))
+(println (conta []))
+
+(defn minha-funcao
+      ([parametro1] (println "p1" parametro1))
+      ([parametro1 parametro2] (println "p2" parametro1 parametro2))
+      )
+
+(minha-funcao 1)
+(minha-funcao 1 2)
+
+
+(defn nova-contagem
+      [elementos]
+      (loop [total-ate-agora 0
+             elementos-restantes
+             elementos]
+        (if (seq elementos-restantes)
+          (recur (inc total-ate-agora) (next elementos-restantes))
+          total-ate-agora)))
+
+(println "nova contagem com elementos" (nova-contagem ["daniela" "guilherme" "carlos" "paulo" "lucia" "ana"]))
+(println "nova contagem sem elementos" (nova-contagem []))
